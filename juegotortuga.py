6dd6edc2438f47ede25291bec3425bb651d26014
Rafa <moducis5@gmail.com>
2016-10-28 17:55:44 +0200

    Añadir movimiento a las tortugas
    He añadido movimiento a las tortugas, para ello me he
    creado en la clase tortuga una funcion para bajar las
    tortugas, otra para generar un numero aleatorio que
    usaremos como tiempo en segundos. A parte le he añadido
    algunas propiedades a las funciones. Luego en en el
    main me he creado un array, y segun la posicion del
    del mismo he generado un numero distinto para cada
    tortuga. Luego he creado un contador que resta 1 cada
    un segundo y segun el tiempo que le quede a cada tortuga
    hace, baja, sube o genera un nuevo numero si ha llegado
    a cero.

--- juegotortuga.py
+++ juegotortuga.py
@@ -1,5 +1,8 @@
 import pygame
+from pygame.locals import *
 from pygame.constants import RLEACCEL
+from time import sleep
+
 
 import patricio
 import bob
@@ -36,35 +39,84 @@ def main():
     pygame.display.set_icon(ico)    
     fondo=cargar_imagen("recursos2d/fondo1.jpg")
     
-    pantalla.blit(fondo,(0,0))
+    
     tortuga1 = tortuga.Tortuga()
     tortuga2 = tortuga.Tortuga()
     tortuga3 = tortuga.Tortuga()
     patricio1= patricio.Patricio()
     bob1 = bob.Bob()
-    tortuga1.redimensionar(95, 95)
-    tortuga2.redimensionar(95, 95)
-    tortuga3.redimensionar(95, 95)
     patricio1.redimensionar(150, 150)
     bob1.redimensionar(90, 90)
     
     
      
+    nume2 = []
+    nume2.append(tortuga1.gene())
+    #print ("numero generado tor1 "+ str(nume2[0]))
+    nume2.append(tortuga2.gene())
+    #print ("numero generado tor2 "+ str(nume2[1]))
+    nume2.append(tortuga3.gene())
+    #print ("numero generado tor3 "+ str(nume2[2])) 
+     
+    
     salir=False
-    reloj=pygame.time.Clock()
+    clock = pygame.time.Clock()
+    pygame.time.set_timer(USEREVENT+1, 1000)
  
     while salir!=True:
+        time = clock.tick(60)
         for event in pygame.event.get(): 
             if event.type==pygame.QUIT or event.type==pygame.K_ESCAPE:
                 salir=True
-                   
-        pygame.display.update()  
-        reloj.tick(20)
+            if event.type == USEREVENT+1:
+                
+                for number in range(1,4):
+                    if nume2[number-1] > 0:
+                        if number == 1:
+                            nume2[number-1]-= 1
+                            #print("tor1:"+str(nume2[number-1]))
+                        if number == 2:
+                            nume2[number-1]-= 1
+                            #print("tor2:"+str(nume2[number-1]))
+                        if number == 3:
+                            nume2[number-1]-= 1
+                            #print("tor3:"+str(nume2[number-1]))
+                            
+        for number in range(1,4):
+            if nume2[number-1] == 1:
+                if number == 1:
+                    tortuga1.bajar(time)
+                if number == 2:
+                    tortuga2.bajar(time)
+                if number == 3:
+                    tortuga3.bajar(time)
+
+            if nume2[number-1] == 0:
+                if number == 1:
+                    tortuga1.__init__()
+                    nume2[0]=tortuga1.gene()
+                    #print("tor1 generado:"+str(nume2[0]))
+                    
+                if number == 2:
+                    tortuga2.__init__()
+                    nume2[1]=tortuga2.gene()
+                    #print("tor2 generado:"+str(nume2[1]))
+                    
+                if number == 3:
+                    tortuga3.__init__()
+                    nume2[2]=tortuga3.gene()
+                    #print("tor2 generado:"+str(nume2[2]))
+        
+        pantalla.blit(fondo,(0,0))
         patricio1.colocar(pantalla, 570, 175)
         bob1.colocar(pantalla, 170 , 210)
-        tortuga1.colocar(pantalla, 260, 300)
-        tortuga2.colocar(pantalla, 385, 300)
-        tortuga3.colocar(pantalla, 510, 300)
+        tortuga1.redimensionar(95, 95)
+        tortuga2.redimensionar(95, 95)
+        tortuga3.redimensionar(95, 95)
+        tortuga1.colocar(pantalla, 260, tortuga1.rect.centery)
+        tortuga2.colocar(pantalla, 385, tortuga2.rect.centery)
+        tortuga3.colocar(pantalla, 510, tortuga3.rect.centery)
+        pygame.display.update()
      
     pygame.quit()  
 
--- tortuga.py
+++ tortuga.py
@@ -1,16 +1,28 @@
 import pygame
+import random
 
 class Tortuga(pygame.sprite.Sprite):
     def __init__(self):
       
         pygame.sprite.Sprite.__init__(self)
         self.ImagenTortuga=pygame.image.load("recursos2d/tortuga1.png").convert_alpha()
+        self.rect = self.ImagenTortuga.get_rect()
+        self.rect.centery = 270
+        self.speed = [0.5, -0.5]
     
     def colocar(self,pantalla,posicionX,posicionY):
         pantalla.blit(self.ImagenTortuga,(posicionX,posicionY))
     def redimensionar(self,ancho,alto):
         self.ImagenTortuga=pygame.transform.scale(self.ImagenTortuga, (ancho, alto))
-    def bajar(self):
-        pass
+        
+    def bajar(self, time):
+
+        if self.rect.centery <= 430:
+            self.rect.centery -= self.speed[1] * time
+            
     def subir(self):
         pass
+    
+    def gene(self):
+        numero = random.randint(4,15)
+        return numero
